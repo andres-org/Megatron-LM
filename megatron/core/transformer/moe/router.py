3baf71f62ab4e8898c80775bcd232d581bd0d6e3
Andres Nowak <andresnowak52@gmail.com>
2026-03-23 18:32:36 +0100

    padding mask for expert max violation mbs computation

--- megatron/core/transformer/moe/router.py
+++ megatron/core/transformer/moe/router.py
@@ -636,6 +636,10 @@ class TopKRouter(Router):
                 num_layers = self.config.num_layers
                 if self.config.mtp_num_layers is not None:
                     num_layers += self.config.mtp_num_layers
+
+                if padding_mask is not None:
+                    routing_map = routing_map & (~padding_mask.unsequeeze(-1))
+
                 tokens_per_expert = routing_map.sum(dim=0).float()
                 max_violation = expert_max_violation_batchwise(
                     tokens_per_expert=tokens_per_expert,
